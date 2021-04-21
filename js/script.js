98e71cb5add3679f545a9e5176684882525429ac
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-04-21 16:12:49 +0200

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+// Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
 var app = new Vue(
     {
         el:'#root',
     
         data:{
+            inputUser:'',
             todos:[
                 'Fare i compiti',
                 'Fare il bucato',
@@ -11,7 +17,17 @@ var app = new Vue(
             ],
         },
         methods:{
+            newToDo(){
+                if(this.inputUser.length > 0){
+                    this.todos.push(this.inputUser);
+                    this.inputUser= '';
+                }
+            },
+            deleteItem(index){
+                this.todos.splice(index,1);
+            }
 
+            
         }
     }
 );

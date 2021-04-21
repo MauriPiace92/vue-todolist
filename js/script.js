fd653fe2a4d3d3a09be087890e0beb5d405f6442
MauriPiace92 <75382063+MauriPiace92@users.noreply.github.com>
2021-04-21 15:33:26 +0200

    Finito layout pagina con html

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue(
+    {
+        el:'#root',
+    
+        data:{
+            todos:[
+                'Fare i compiti',
+                'Fare il bucato',
+                'Fare la spesa'
+
+            ],
+        },
+        methods:{
+
+        }
+    }
+);
